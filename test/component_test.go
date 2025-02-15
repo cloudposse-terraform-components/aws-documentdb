632365bfb104e27ede4360f28f4e21a61d9b85ad
Igor Rodionov <goruha@gmail.com>
2025-02-15 19:24:54 +0100

    Added tests

--- test/component_test.go
+++ test/component_test.go
@@ -7,129 +7,104 @@ import (
 
 	"github.com/aws/aws-sdk-go-v2/service/docdb"
 	"github.com/cloudposse/test-helpers/pkg/atmos"
-	helper "github.com/cloudposse/test-helpers/pkg/atmos/aws-component-helper"
+	helper "github.com/cloudposse/test-helpers/pkg/atmos/component-helper"
+	awshelper "github.com/cloudposse/test-helpers/pkg/aws"
 	"github.com/gruntwork-io/terratest/modules/aws"
 	"github.com/gruntwork-io/terratest/modules/random"
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 )
 
-func TestComponent(t *testing.T) {
-	// Define the AWS region to use for the tests
-	awsRegion := "us-east-2"
-
-	// Initialize the test fixture
-	fixture := helper.NewFixture(t, "../", awsRegion, "test/fixtures")
-
-	// Ensure teardown is executed after the test
-	defer fixture.TearDown()
-	fixture.SetUp(&atmos.Options{})
-
-	// Define the test suite
-	fixture.Suite("default", func(t *testing.T, suite *helper.Suite) {
-		suite.AddDependency("vpc", "default-test")
-
-		// Setup phase: Create DNS zones for testing
-		suite.Setup(t, func(t *testing.T, atm *helper.Atmos) {
-			basicDomain := "components.cptest.test-automation.app"
-
-			// Deploy the delegated DNS zone
-			inputs := map[string]interface{}{
-				"zone_config": []map[string]interface{}{
-					{
-						"subdomain": suite.GetRandomIdentifier(),
-						"zone_name": basicDomain,
-					},
-				},
-			}
-			atm.GetAndDeploy("dns-delegated", "default-test", inputs)
-		})
-
-		// Teardown phase: Destroy the DNS zones created during setup
-		suite.TearDown(t, func(t *testing.T, atm *helper.Atmos) {
-			// Deploy the delegated DNS zone
-			inputs := map[string]interface{}{
-				"zone_config": []map[string]interface{}{
-					{
-						"subdomain": suite.GetRandomIdentifier(),
-						"zone_name": "components.cptest.test-automation.app",
-					},
-				},
-			}
-			atm.GetAndDestroy("dns-delegated", "default-test", inputs)
-		})
-
-		// Test phase: Validate the functionality of the ALB component
-		suite.Test(t, "basic", func(t *testing.T, atm *helper.Atmos) {
-			name := strings.ToLower(random.UniqueId())
-			userName := strings.ToLower(random.UniqueId())
-			inputs := map[string]interface{}{
-				"master_username": userName,
-				"name":            name,
-			}
-
-			defer atm.GetAndDestroy("documentdb/basic", "default-test", inputs)
-			component := atm.GetAndDeploy("documentdb/basic", "default-test", inputs)
-			assert.NotNil(t, component)
-
-			masterUsername := atm.Output(component, "master_username")
-			assert.Equal(t, userName, masterUsername)
-
-			clusterName := atm.Output(component, "cluster_name")
-			assert.NotEmpty(t, clusterName)
-
-			arn := atm.Output(component, "arn")
-			assert.NotEmpty(t, arn)
-
-			endpoint := atm.Output(component, "endpoint")
-			assert.NotEmpty(t, endpoint)
-
-			readerEndpoint := atm.Output(component, "reader_endpoint")
-			assert.NotEmpty(t, readerEndpoint)
-
-			masterEndpoint := atm.Output(component, "master_host")
-			assert.NotEmpty(t, masterEndpoint)
-
-			replicasHost := atm.Output(component, "replicas_host")
-			assert.NotEmpty(t, replicasHost)
-
-			securityGroupId := atm.Output(component, "security_group_id")
-			assert.NotEmpty(t, securityGroupId)
-
-			securityGroupArn := atm.Output(component, "security_group_arn")
-			assert.NotEmpty(t, securityGroupArn)
-
-			securityGroupName := atm.Output(component, "security_group_name")
-			assert.NotEmpty(t, securityGroupName)
-
-			client := NewDocDBlient(t, awsRegion)
-			clusters, err := client.DescribeDBClusters(context.Background(), &docdb.DescribeDBClustersInput{
-				DBClusterIdentifier: &clusterName,
-			})
-			assert.NoError(t, err)
-			assert.Equal(t, arn, *clusters.DBClusters[0].DBClusterArn)
-			assert.Equal(t, clusterName, *clusters.DBClusters[0].DBClusterIdentifier)
-			assert.Equal(t, readerEndpoint, *clusters.DBClusters[0].ReaderEndpoint)
-			assert.Equal(t, securityGroupId, *clusters.DBClusters[0].VpcSecurityGroups[0].VpcSecurityGroupId)
-
-			delegatedDnsZoneId := atm.Output(helper.NewAtmosComponent("dns-delegated", "default-test", map[string]interface{}{}), "default_dns_zone_id")
-			masterEndpointDNSRecord := aws.GetRoute53Record(t, delegatedDnsZoneId, masterEndpoint, "CNAME", awsRegion)
-			assert.Equal(t, *masterEndpointDNSRecord.ResourceRecords[0].Value, *clusters.DBClusters[0].Endpoint)
-		})
-	})
+type ComponentSuite struct {
+	helper.TestSuite
 }
 
-func NewDocDBlient(t *testing.T, region string) *docdb.Client {
-	client, err := NewDocDBlientE(t, region)
-	require.NoError(t, err)
+func (s *ComponentSuite) TestBasic() {
+	const component = "documentdb/basic"
+	const stack = "default-test"
+	const awsRegion = "us-east-2"
 
-	return client
-}
-
-func NewDocDBlientE(t *testing.T, region string) (*docdb.Client, error) {
-	sess, err := aws.NewAuthenticatedSession(region)
-	if err != nil {
-		return nil, err
+	name := strings.ToLower(random.UniqueId())
+	userName := strings.ToLower(random.UniqueId())
+	inputs := map[string]interface{}{
+		"master_username": userName,
+		"name":            name,
 	}
-	return docdb.NewFromConfig(*sess), nil
+
+	defer s.DestroyAtmosComponent(s.T(), component, stack, &inputs)
+	options, _ := s.DeployAtmosComponent(s.T(), component, stack, &inputs)
+	assert.NotNil(s.T(), options)
+
+	masterUsername := atmos.Output(s.T(), options, "master_username")
+	assert.Equal(s.T(), userName, masterUsername)
+
+	clusterName := atmos.Output(s.T(), options, "cluster_name")
+	assert.NotEmpty(s.T(), clusterName)
+
+	arn := atmos.Output(s.T(), options, "arn")
+	assert.NotEmpty(s.T(), arn)
+
+	endpoint := atmos.Output(s.T(), options, "endpoint")
+	assert.NotEmpty(s.T(), endpoint)
+
+	readerEndpoint := atmos.Output(s.T(), options, "reader_endpoint")
+	assert.NotEmpty(s.T(), readerEndpoint)
+
+	masterEndpoint := atmos.Output(s.T(), options, "master_host")
+	assert.NotEmpty(s.T(), masterEndpoint)
+
+	replicasHost := atmos.Output(s.T(), options, "replicas_host")
+	assert.NotEmpty(s.T(), replicasHost)
+
+	securityGroupId := atmos.Output(s.T(), options, "security_group_id")
+	assert.NotEmpty(s.T(), securityGroupId)
+
+	securityGroupArn := atmos.Output(s.T(), options, "security_group_arn")
+	assert.NotEmpty(s.T(), securityGroupArn)
+
+	securityGroupName := atmos.Output(s.T(), options, "security_group_name")
+	assert.NotEmpty(s.T(), securityGroupName)
+
+	client := awshelper.NewDocDBClient(s.T(), awsRegion)
+	clusters, err := client.DescribeDBClusters(context.Background(), &docdb.DescribeDBClustersInput{
+		DBClusterIdentifier: &clusterName,
+	})
+	assert.NoError(s.T(), err)
+	assert.Equal(s.T(), arn, *clusters.DBClusters[0].DBClusterArn)
+	assert.Equal(s.T(), clusterName, *clusters.DBClusters[0].DBClusterIdentifier)
+	assert.Equal(s.T(), readerEndpoint, *clusters.DBClusters[0].ReaderEndpoint)
+	assert.Equal(s.T(), securityGroupId, *clusters.DBClusters[0].VpcSecurityGroups[0].VpcSecurityGroupId)
+
+	dnsDelegatedOptions := s.GetAtmosOptions("dns-delegated", "default-test", nil)
+	delegatedDnsZoneId := atmos.Output(s.T(), dnsDelegatedOptions, "default_dns_zone_id")
+	masterEndpointDNSRecord := aws.GetRoute53Record(s.T(), delegatedDnsZoneId, masterEndpoint, "CNAME", awsRegion)
+	assert.Equal(s.T(), *masterEndpointDNSRecord.ResourceRecords[0].Value, *clusters.DBClusters[0].Endpoint)
+
+	s.DriftTest(component, stack, &inputs)
+}
+
+func (s *ComponentSuite) TestEnabledFlag() {
+	const component = "documentdb/disabled"
+	const stack = "default-test"
+	const awsRegion = "us-east-2"
+
+	s.VerifyEnabledFlag(component, stack, nil)
+}
+
+
+func TestRunSuite(t *testing.T) {
+	suite := new(ComponentSuite)
+
+	suite.AddDependency(t, "vpc", "default-test", nil)
+
+	subdomain := strings.ToLower(random.UniqueId())
+	inputs := map[string]interface{}{
+		"zone_config": []map[string]interface{}{
+			{
+				"subdomain": subdomain,
+				"zone_name": "components.cptest.test-automation.app",
+			},
+		},
+	}
+	suite.AddDependency(t, "dns-delegated", "default-test", &inputs)
+	helper.Run(t, suite)
 }
